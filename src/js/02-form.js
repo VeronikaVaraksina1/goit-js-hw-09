582858ba53fdf3cb8fdda52f32b8eee1c940418b
VeronikaVaraksina1 <plaguemoon@gmail.com>
2023-12-19 16:00:31 +0200

    tasks updated

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -7,21 +7,31 @@ const localStorageKey = 'feedback-form-state';
 const input = feedbackForm.elements.email;
 const textarea = feedbackForm.elements.message;
 
-const form = {
-  email: input.value,
-  message: textarea.value,
-};
+const savedState = localStorage.getItem(localStorageKey);
 
-input.value = localStorage.getItem(localStorageKey) ?? '';
-textarea.value = localStorage.getItem(localStorageKey) ?? '';
+if (savedState ?? '') {
+  const parsedState = JSON.parse(savedState);
+  input.value = parsedState.email;
+  textarea.value = parsedState.message;
+}
 
 feedbackForm.addEventListener('input', () => {
-  localStorage.setItem(localStorageKey, JSON.stringify(form));
+  const feedback = {
+    email: input.value.trim(),
+    message: textarea.value.trim(),
+  };
+
+  localStorage.setItem(localStorageKey, JSON.stringify(feedback));
 });
 
 feedbackForm.addEventListener('submit', e => {
-  evt.preventDefault();
-  console.log(evt.target.elements.message.value);
+  e.preventDefault();
+
+  if (input.value === '' || textarea.value === '') {
+    alert('You need to write a message!');
+    return;
+  }
+
   localStorage.removeItem(localStorageKey);
-  form.reset();
+  feedbackForm.reset();
 });
